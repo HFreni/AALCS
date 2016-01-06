296d78ce8dd917fb4298ceb0553f8477ac39b5a7
Harrison Freni <harrison@harrisonfreni.me>
2016-01-05 20:18:24 -0800

    Refactored, and simplified code

--- lightsOn.py
+++ lightsOn.py
@@ -1,8 +1,19 @@
+import sys
 import requests
 
-token = #Token Goes Here
+payload = {"power": "on", "color": "rgb:255,255,255", "brightness": 0.50}
+
+token = sys.argv[1]
+
 r = requests.get('https://api.lifx.com/v1/lights/all', auth=(token, ''))
+c = requests.put('https://api.lifx.com/v1/lights/all/state', data=payload,  auth=(token, ''))
+
+print c.status_code
+print c.headers
+print c.content
+
 
 print r.status_code
 print r.headers
 print r.content
+
