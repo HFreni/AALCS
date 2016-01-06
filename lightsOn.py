ad42859c7dc4e532b7fc8d58a066a8a7d5480991
Harrison Freni <harrison@harrisonfreni.me>
2016-01-05 20:34:51 -0800

    Modified Code, Functions

--- lightsOn.py
+++ lightsOn.py
@@ -6,12 +6,26 @@ payload = {"power": "on", "color": "rgb:255,255,255", "brightness": 0.50}
 token = sys.argv[1]
 
 r = requests.get('https://api.lifx.com/v1/lights/all', auth=(token, ''))
-c = requests.put('https://api.lifx.com/v1/lights/all/state', data=payload,  auth=(token, ''))
 
-print c.status_code
-print c.headers
-print c.content
+def allon():
+    payload = {"power": "on", "color": "rgb:255,255,255",  "brightness": 0.80}
+    c = requests.put('https://api.lifx.com/v1/lights/all/state', data=payload,  auth=(token, ''))
+    print c.status_code
+    print c.headers
+    print c.content
+    print payload
 
+def rom():
+    new = {"power": "on", "color": "rgb:255,0,0", "brightness": 0.50}
+    g = requests.put('https://api.lifx.com/v1/lights/all/state', data=new, auth=(token, ''))
+    print g.status_code
+    print g.headers
+    print g.content
+    print new
+
+
+allon()
+rom()
 
 print r.status_code
 print r.headers
